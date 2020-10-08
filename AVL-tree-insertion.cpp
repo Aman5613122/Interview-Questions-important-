d4b02ca2452003aad8ced440c92090449a10e283
Aman5613122 <47296280+Aman5613122@users.noreply.github.com>
2020-10-08 10:33:04 +0530

    Update AVL-tree-insertion.cpp

--- AVL-tree-insertion.cpp
+++ AVL-tree-insertion.cpp
@@ -123,6 +123,118 @@ Node* avl(Node *root,int val)
     return root;
 }
 
+void inorder(Node *root)
+{
+    if(!root)
+        return;
+    cout << root->data << " ";
+    inorder(root->left);
+    inorder(root->right);
+}
+
+Node *minvalue(Node *curr)
+{
+    while(curr->left != NULL)
+    {
+        curr= curr->left;
+    }
+    return curr;
+}
+
+Node *del(Node *root,int key)
+{
+    if(!root)
+    {
+        return root;
+    }
+
+    if(root->data < key)
+    {
+        root->right = del(root->right,key);
+    }
+    else if(root->data > key)
+    {
+        root->left = del(root->left,key);
+    }
+    else{
+        //if key has only two child
+
+        if(root->left == NULL || root->right == NULL)
+        {
+            Node *temp = NULL;
+            temp = root->left?root->left:root->right;
+
+            if(temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else{
+                root->data = temp->data;
+                root->left = NULL;
+                root->right = NULL;
+            }
+            delete(temp);
+        }
+        else
+        {
+            // node with two children: Get the inorder
+            // successor (smallest in the right subtree)
+            Node* temp = minvalue(root->right);
+
+            // Copy the inorder successor's
+            // data to this node
+            root->data = temp->data;
+
+            // Delete the inorder successor
+            root->right = del(root->right,
+                                     temp->data);
+        }
+    }
+
+    if(!root)
+    {
+        return root;
+    }
+
+        //find the height of the tree
+
+    root->height = 1 + max(height(root->left),height(root->right));
+
+    //find if the tree is balanced or not
+
+    int balanced = getbalance(root);
+
+    //Now check which case is apply on it
+
+    //left-left case
+    if (balanced > 1 &&
+        getbalance(root->left) >= 0)
+        return rightrotation(root);
+
+    // Left Right Case
+    if (balanced > 1 &&
+        getbalance(root->left) < 0)
+    {
+        root->left = leftrotation(root->left);
+        return rightrotation(root);
+    }
+
+    // Right Right Case
+    if (balanced < -1 &&
+        getbalance(root->right) <= 0)
+        return leftrotation(root);
+
+    // Right Left Case
+    if (balanced < -1 &&
+        getbalance(root->right) > 0)
+    {
+        root->right = rightrotation(root->right);
+        return leftrotation(root);
+    }
+
+    return root;
+}
 
 int main()
 {
@@ -134,6 +246,10 @@ int main()
     root = avl(root,50);
     root = avl(root,25);
 
+    inorder(root);
+
+    del(root,40);
+    cout << endl;
+    inorder(root);
     return 0;
 }
-
